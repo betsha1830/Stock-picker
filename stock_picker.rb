a492d222ffa6cf7b572755032483d431ef7b4fd5
Betsue Weldemariam <btsegay0@gmail.com>
2022-03-22 11:40:21 +0300

    Code clean up.

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,13 +1,3 @@
-# PROBLEM
-# Find the most profitable days to buy and sell a certain stock.
-
-# CASES
-# 1- Days to buy at a minimum and sell at a maximum
-
-# SOLUTION
-# 1- Take the most least number and take the most greatest number from its right.
-# [17,3,6,9,15,8,6,1,10]
-
 def stock_picker (stock_price)
 
     price_diff = []
@@ -24,5 +14,3 @@ def stock_picker (stock_price)
     price_pair[price_diff.find_index(price_diff.max)]
 
 end
-
-puts stock_picker [17,3,6,9,15,8,6,1,10]
