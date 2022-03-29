5012a7682ebbd52c134592db20e0c20b17942de4
Betsue Weldemariam <btsegay0@gmail.com>
2022-03-29 13:06:42 +0300

    Code refractor using rubocop.

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,16 +1,14 @@
-def stock_picker (stock_price)
+def stock_picker(stock_price)
+  price_diff = []
+  price_pair = []
 
-    price_diff = []
-    price_pair = []
-
-    stock_price.each_with_index do |item,index|
-        stock_price.each_with_index do |value, i|
-            if (value > item && i > index)
-                price_diff.push(value-item)
-                price_pair.push([index,i])
-            end
-        end
+  stock_price.each_with_index do |item, index|
+    stock_price.each_with_index do |value, i|
+      if value > item && i > index
+        price_diff.push(value - item)
+        price_pair.push([index, i])
+      end
     end
-    price_pair[price_diff.find_index(price_diff.max)]
-
+  end
+  price_pair[price_diff.find_index(price_diff.max)]
 end
